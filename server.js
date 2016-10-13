ff044e8899b6d95254b95635491c9d8985634eb9
deepika61287 <deeps61287@gmail.com>
2016-10-13 13:03:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,91 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = { 
+  title : 'Article One | Deepika',
+  heading : 'Article One',
+  date : 'October 13,2016',
+  content : `
+  <p> This is for article one html page..This is the additional html file which is linked to the main html page </p>
+            
+  <p> GitHub lets you version your code, collaborate with others, and share what you have built with the world. We are excited to see what you make. </p>
+            
+  <p> Ready to put your first open source project on GitHub? Get started by creating a repository for your work to live in. A repository will let you version your work, open issues to track bugs or tasks, and invite contributors to work with you. </p>
+  `
+};
+
+function createTemplate(data)
+    {
+
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate =  `
+      
+      <html>
+        
+        <head>
+            
+            ${title}
+            
+            <meta name = "viewport" content="width=device-width" initial-scaled ="1"> 
+            
+            <link href="/ui/style.css" rel="stylesheet" />
+            
+        </head>
+        
+        <body>
+            
+            <div class = "container">
+            
+                <div>
+                
+                    <a href = '/' > Home </a>
+                    
+                
+                </div>
+            
+                <hr/>
+            
+                <div>
+                
+                    ${heading}   
+                
+                </div>
+            
+                <div>
+                
+                    ${date}
+                
+                </div>
+            
+                <div>
+                
+                    ${content}
+                
+                </div>
+            
+            </div>
+            
+        </body>
+        
+    </html>
+    
+    
+      `;
+      
+      return htmlTemplate;
+      
+    }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res) {

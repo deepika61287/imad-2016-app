c1326f54e5f25d150a340d33adb57ef1065b782c
deepika61287 <deeps61287@gmail.com>
2016-10-14 00:46:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,8 +47,8 @@ button.onclick = function() {
     
     //capture the name and display the set of names submitted
     var names = ['name1','name2','name3','name4'];
-    var list ='';
-    for (var i=0;i < names.length;i++)
+    var list = '';
+    for (var i = 0; i < names.length; i++)
     {
         list+= '<li>' + names[i] + '</li>'
         console.log(names[i]);

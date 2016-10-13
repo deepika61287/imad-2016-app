b2fb602b7fbabffb1faf5343d8311e98bee5659d
deepika61287 <deeps61287@gmail.com>
2016-10-13 23:04:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,22 @@ img.onclick = function() {
     //img.style.marginLeft = '100px';
 };
 
+var button = document.getElementById('counter');
+button.onclick = function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if(readystatechange === XMLHttpRequest.DONE)
+    {
+        if(request.status === 200)
+        {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+  };
+  
+  request.open('GET','http://deepika61287.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
+};

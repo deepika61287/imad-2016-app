43de733e530738e139b2634033650818c8eceaa6
deepika61287 <deeps61287@gmail.com>
2016-10-14 01:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/submit-name' , function (req , res) { //submit-name?name=xxxx
    var name = req.query.name;
    names.push(name);
    //JSON
-   res.send(JSON.stringify(names));
+   res.send(JSON.stringify(name));
 });
 
 var counter = 0;

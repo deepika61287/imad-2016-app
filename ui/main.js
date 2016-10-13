f7181a4a0f92b168fe252f0617cc3a003a018b73
deepika61287 <deeps61287@gmail.com>
2016-10-13 21:00:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = "WOW!!!";
 
 //move
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };

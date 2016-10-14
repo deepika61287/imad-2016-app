161a7666e7c568c6ecc5c49d4be3f16bc28abca6
deepika61287 <deeps61287@gmail.com>
2016-10-14 13:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,8 +41,7 @@ button.onclick = function() {
   
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
@@ -68,6 +67,8 @@ submit.onclick = function() {
     }
 };
   
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET','http://deepika61287.imad.hasura-app.io/submit-name?name' + name,true);
   request.send(null);
   
